a31db3163f7c8acf5f975c13d283ef57680c9012
SashaCHU-st <alexandra.churina@gmail.com>
2026-01-29 10:46:10 +0200

    checking API, showing pictures of exercises

---
+++ client/src/components/ui/primitives/ColumnContainer.tsx
@@ -0,0 +1,9 @@
+import { styled } from "@linaria/react";
+export const ColumnContainer = styled.div`
+  width: 100%;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+`;
